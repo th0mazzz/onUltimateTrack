c93c8cf42460741907e5de5d79cde157e0b92e44
th0mazzz <thomaszhao2001@gmail.com>
2019-05-13 00:09:10 -0400

    Very basic app.routes and templates re. login/register to get us started

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def func():
-    return 'do something'
+def landing():
+    return render_template('base.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/auth', methods=["POST"])
+def auth():
+    return "this is supposed to authenticate user"
 if __name__ == '__main__':
     app.debug = True
     app.run()
